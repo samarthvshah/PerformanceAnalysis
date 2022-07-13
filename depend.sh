1201489d9fd88b6c8c8a85e5e8bdbfa67b8a48a8
Samarth <40899259+samarthvshah@users.noreply.github.com>
2022-07-13 11:22:17 -0700

    updated dependency log file output

--- depend.sh
+++ depend.sh
@@ -5,12 +5,19 @@ date=`date +"%m-%d-%y_%T"`
 has_multi="true"
 
 
+# Create log file
+echo "Dependency Check" > log/dependency_log_file_$date.txt
+echo "Date: $date\n" >> log/dependency_log_file_$date.txt
+
+
 # Function to check if a certain needed command is available, installing it if not there
 checkfor() {
 	if command -v $1 >/dev/null 2>&1 ; then
 		echo "$1 found"
+		echo "$1 found\n" >> log/dependency_log_file_$date.txt
 	else
 		echo "$1 not found, installing now"
+		echo "$1 not found, installing now" >> log/dependency_log_file_$date.txt
 		sudo apt-get -y install $1 >> log/dependency_log_file_$date.txt
 		echo "\n" >> log/dependency_log_file_$date.txt
 	fi
@@ -21,8 +28,10 @@ checkfor() {
 checkforfile() {
 	if [ -f $2 ]; then
 		echo "$1 found"
+		echo "$1 found\n" >> log/dependency_log_file_$date.txt
 	else
 		echo "$1 not found, must be installed"
+		echo "$1 not found, must be installed\n" >> log/dependency_log_file_$date.txt
 		has_multi="false"
 	fi		
 }
@@ -33,8 +42,10 @@ checkforfile() {
 # cat is part of the coreutils package
 if command -v cat >/dev/null 2>&1 ; then
 	echo "cat found"
+	echo "cat found\n" >> log/dependency_log_file_$date.txt
 else
 	echo "cat not found, installing now"
+	echo "cat not found, installing now" >> log/dependency_log_file_$date.txt
 	sudo apt-get -y install coreutils >> log/dependency_log_file_$date.txt
 	echo "\n" >> log/dependency_log_file_$date.txt
 fi
@@ -42,8 +53,10 @@ fi
 # Lscpu is part of the util-linux package
 if command -v lscpu >/dev/null 2>&1 ; then
 	echo "lscpu found"
+	echo "lscpu found\n" >> log/dependency_log_file_$date.txt
 else
 	echo "lscpu not found, installing now"
+	echo "lscpu not found, installing now" >> log/dependency_log_file_$date.txt
 	sudo apt-get -y install util-linux >> log/dependency_log_file_$date.txt
 	echo "\n" >> log/dependency_log_file_$date.txt
 fi
@@ -51,8 +64,10 @@ fi
 # nproc is part of the coreutils package
 if command -v nproc >/dev/null 2>&1 ; then
 	echo "nproc found"
+	echo "nproc found\n" >> log/dependency_log_file_$date.txt
 else
 	echo "nproc not found, installing now"
+	echo "nproc not found, installing now" >> log/dependency_log_file_$date.txt
 	sudo apt-get -y install coreutils >> log/dependency_log_file_$date.txt
 	echo "\n" >> log/dependency_log_file_$date.txt
 fi
@@ -71,8 +86,10 @@ checkfor "numactl"
 # Lstopo is installed using hwloc instead of its name 
 if command -v lstopo >/dev/null 2>&1 ; then
 	echo "lstopo found"
+	echo "lstopo found\n" >> log/dependency_log_file_$date.txt
 else
 	echo "lstopo not found, installing now"
+	echo "lstopo not found, installing now" >> log/dependency_log_file_$date.txt
 	sudo apt-get -y install hwloc >> log/dependency_log_file_$date.txt
 	echo "\n" >> log/dependency_log_file_$date.txt
 fi
@@ -80,8 +97,10 @@ fi
 # Cpanm is installed using cpanminus instead of its name 
 if command -v cpanm >/dev/null 2>&1 ; then
 	echo "cpanm found"
+	echo "cpanm found\n" >> log/dependency_log_file_$date.txt
 else
 	echo "cpanm not found, installing now"
+	echo "cpanm not found, installing now" >> log/dependency_log_file_$date.txt
 	sudo apt-get -y install cpanminus >> log/dependency_log_file_$date.txt
 	echo "\n" >> log/dependency_log_file_$date.txt
 fi
@@ -99,18 +118,21 @@ checkfor "ipmitool"
 # Checking for Stream files
 if [ -f "src/Stream/stream" ]; then
 	echo "stream found, still must be recompiled"
+	echo "stream found, still must be recompiled\n" >> log/dependency_log_file_$date.txt
 else
 	echo "stream not found, must be manually installed"
-	mkdir src/Stream
+	echo "stream not found, must be manually installed\n" >> log/dependency_log_file_$date.txt
+	mkdir src/Stream >> log/dependency_log_file_$date.txt
 	
 	if [ ! -f "src/Stream/stream.c" ]
 	then
-		wget -q -P src/Stream cs.virginia.edu/stream/FTP/Code/stream.c
+		wget -q -P src/Stream cs.virginia.edu/stream/FTP/Code/stream.c >> log/dependency_log_file_$date.txt
 	fi
 fi	
 
 # Recompiling stream with the gcc compiler on the system (precompiled does not work)
-gcc -fopenmp -D_OPENMP src/Stream/stream.c -o src/Stream/stream
+echo "compiling stream"
+gcc -fopenmp -D_OPENMP src/Stream/stream.c -o src/Stream/stream >> log/dependency_log_file_$date.txt
 
 
 # Checking for multichase files
@@ -120,15 +142,15 @@ checkforfile "multichase fairness" "src/multichase/fairness"
 checkforfile "multichase pingpong" "src/multichase/pingpong"
 
 if [ $has_multi = "false" ]; then
-	rm -rf "src/multichase"
-	cd src/
-	git clone https://github.com/google/multichase.git
-	cd multichase/
-	make
-	cd ..
-	cd ..
+	echo "multichase needs to be reinstalled, doing that now"
+	echo "doing multichase reinstall" >> log/dependency_log_file_$date.txt
+	rm -rf "src/multichase" >> log/dependency_log_file_$date.txt
+	git clone https://github.com/google/multichase.git src/multichase >> log/dependency_log_file_$date.txt
+	make -C src/multichase >> log/dependency_log_file_$date.txt
+	echo "\n" >> log/dependency_log_file_$date.txt
 fi
 	
 
 # Excel::Writer::XLSX
+echo "Checking for Excel::Writer::XLSX" >> log/dependency_log_file_$date.txt
 sudo cpanm Excel::Writer::XLSX
