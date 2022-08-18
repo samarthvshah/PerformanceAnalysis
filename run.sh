226ba35132234a0450674be80c40a33ffd32f634
Samarth <40899259+samarthvshah@users.noreply.github.com>
2022-08-18 15:21:21 -0700

    Updated formatting to be easier to use

--- run.sh
+++ run.sh
@@ -2,12 +2,22 @@
 
 # Ask which of the scripts the user would like to run
 echo ""
-read -p "What do you want to run? (sys for only gathering system info, func for functional testing, perf for performance testing, comp for NUMA node comparison testing, default is system info): " run
+echo "1. Gather System Information"
+echo "2. Run Functional Testing"
+echo "3. Gather Performance Data"
+echo "4. Run NUMA Node Comparison Testing"
+echo "___________________________________"
+read -p "What do you want to run?: " run
 
-if [ "$run" = "func" ]; then 
-	sh scripts/functional_test.sh
+if [ "$run" = "2" ]; then 
+	
+	# Ask what platform the system is on
+	echo ""
+	read -p "What platform is the system running on? (intel or amd, default is amd): " platform
 
-elif [ "$run" = "perf" ]; then
+	sh scripts/functional_test.sh "$platform"
+
+elif [ "$run" = "3" ]; then
 
 	# Ask what platform the system is on
 	echo ""
@@ -18,7 +28,16 @@ elif [ "$run" = "perf" ]; then
 	
 		# Ask what workloads to run
 		echo ""
-		read -p "What workloads do you want to run? (enter a comma-seperated list with stream for STREAM, stress for StressAppTest, fio for Flexible I/O tester, multichase for Multichase, lmbench for Lmbench, mlc for Intel Memory Latency Checker, all for All Workloads): " workloads
+		echo "_____________________________________________"
+		echo "| stream     | Stream                       |"
+		echo "| stress     | StressAppTest                |"
+		echo "| fio        | Flexible I/O Tester          |"
+		echo "| multichase | Multichase + Multiload       |"
+		echo "| lmbench    | Lmbench                      |"
+		echo "| mlc        | Intel Memory Latency Checker |"
+		echo "| all        | Run all Workloads            |"
+		echo "|____________|______________________________|"
+		read -p "What workloads do you want to run? (enter a comma-seperated list with the left side names above): " workloads
 		
 		sh scripts/int_performance_test.sh "$workloads"
 		
@@ -26,14 +45,22 @@ elif [ "$run" = "perf" ]; then
 	
 		# Ask what workloads to run
 		echo ""
-		read -p "What workloads do you want to run? (enter a comma-seperated list with stream for STREAM, stress for StressAppTest, fio for Flexible I/O tester, multichase for Multichase, lmbench for Lmbench, all for All Workloads): " workloads
+		echo "_____________________________________________"
+		echo "| stream     | Stream                       |"
+		echo "| stress     | StressAppTest                |"
+		echo "| fio        | Flexible I/O Tester          |"
+		echo "| multichase | Multichase + Multiload       |"
+		echo "| lmbench    | Lmbench                      |"
+		echo "| all        | Run all Workloads            |"
+		echo "|____________|______________________________|"
+		read -p "What workloads do you want to run? (enter a comma-seperated list with the left side names above): " workloads
 		
 		sh scripts/amd_performance_test.sh "$workloads"
 		
 	fi
 		
 	
-elif [ "$run" = "comp" ]; then
+elif [ "$run" = "4" ]; then
 
 	# Ask what platform the system is on
 	echo ""
@@ -44,7 +71,15 @@ elif [ "$run" = "comp" ]; then
 	
 		# Ask what workloads to run
 		echo ""
-		read -p "What workloads do you want to run? (enter a comma-seperated list with stream for STREAM, stress for StressAppTest, fio for Flexible I/O tester, multichase for Multichase, lmbench for Lmbench, mlc for Intel Memory Latency Checker, all for All Workloads): " workloads
+		echo "_____________________________________________"
+		echo "| stream     | Stream                       |"
+		echo "| stress     | StressAppTest                |"
+		echo "| fio        | Flexible I/O Tester          |"
+		echo "| multichase | Multichase + Multiload       |"
+		echo "| mlc        | Intel Memory Latency Checker |"
+		echo "| all        | Run all Workloads            |"
+		echo "|____________|______________________________|"
+		read -p "What workloads do you want to run? (enter a comma-seperated list with the left side names above): " workloads
 		
 		sh scripts/int_comparison.sh "$workloads"
 		
@@ -52,14 +87,25 @@ elif [ "$run" = "comp" ]; then
 	
 		# Ask what workloads to run
 		echo ""
-		read -p "What workloads do you want to run? (enter a comma-seperated list with stream for STREAM, stress for StressAppTest, fio for Flexible I/O tester, multichase for Multichase, lmbench for Lmbench, all for All Workloads): " workloads
+		echo "_____________________________________________"
+		echo "| stream     | Stream                       |"
+		echo "| stress     | StressAppTest                |"
+		echo "| fio        | Flexible I/O Tester          |"
+		echo "| multichase | Multichase + Multiload       |"
+		echo "| all        | Run all Workloads            |"
+		echo "|____________|______________________________|"
+		read -p "What workloads do you want to run? (enter a comma-seperated list with the left side names above): " workloads
 		
 		sh scripts/amd_comparison.sh "$workloads"	
 	
 	fi
 	
 else 
-	sh scripts/system_info.sh
+	# Ask what platform the system is on
+	echo ""
+	read -p "What platform is the system running on? (intel or amd, default is amd): " platform
+	
+	sh scripts/system_info.sh "$platform"
 	
 fi
 
